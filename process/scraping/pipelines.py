ff2f02eb7f265dea4b949a4834c84d509e6621c6
Matthew Spalding <matthew.a.spalding@gmail.com>
2016-01-19 14:41:33 -0500

    Replacing LF (\n) with CRLF (\r\n).

--- process/scraping/pipelines.py
+++ process/scraping/pipelines.py
@@ -8,22 +8,22 @@
 #import json
 
 #class PlayerPipeline(object):
-#	def __init__(self):
-		# Open the file we will write to, and keep it in our object's scope.
-#		self.jsonfileNFLandFBGDicts = open("NFL and FBG Dicts.json", "wb")
-#	
-#	def process_item(self, item, spider):
-		# Write this player item as a dict, ending in a newline.
-#		line = json.dumps(dict(item)) + "\n"
-#		self.jsonfileNFLandFBGDicts.write(line)
-		# Per the docs here: 
-		#	http://doc.scrapy.org/en/latest/topics/item-pipeline.html#process_item 
-		# we must either return the item or raise a DropItem execption.
-#		return item
-#	
-#	def spider_closed():
-		# Just close the file.
-#		self.jsonfileNFLandFBGDicts.close()
+#    def __init__(self):
+        # Open the file we will write to, and keep it in our object's scope.
+#        self.jsonfileNFLandFBGDicts = open("NFL and FBG Dicts.json", "wb")
+#    
+#    def process_item(self, item, spider):
+        # Write this player item as a dict, ending in a newline.
+#        line = json.dumps(dict(item)) + "\n"
+#        self.jsonfileNFLandFBGDicts.write(line)
+        # Per the docs here: 
+        #    http://doc.scrapy.org/en/latest/topics/item-pipeline.html#process_item 
+        # we must either return the item or raise a DropItem execption.
+#        return item
+#    
+#    def spider_closed():
+        # Just close the file.
+#        self.jsonfileNFLandFBGDicts.close()
 
 
 # ------------------------ NEW WAY - To .csv ------------------------
@@ -32,25 +32,25 @@ from scripted.items import NFLPlayer
 
 
 class PlayerPipeline(object):
-	def __init__(self):
-		# Open the file we will write to, and keep it in our object's scope.
-		self.csvNFLandFBGfile = open("NFL and FBG.csv", "wb")
-		# Create our list of field names.
-		listFieldNames = NFLPlayer.fields.keys()
-		# Create our DictWriter with the list of field names as the header row.
-		self.writerPlayerAttributeDicts = csv.DictWriter(self.csvNFLandFBGfile, listFieldNames)
-		# Write the header first.
-		self.writerPlayerAttributeDicts.writeheader()
-		# We're done here. The rest of the writing will be done per-item in process_item.
-	
-	def process_item(self, item, spider):
-		# Write this player item as a dict.
-		self.writerPlayerAttributeDicts.writerow(dict(item))
-		# Per the docs here: 
-		#	http://doc.scrapy.org/en/latest/topics/item-pipeline.html#process_item 
-		# we must either return the item or raise a DropItem execption.
-		return item
-	
-	def spider_closed():
-		# Just close the file.
-		self.csvNFLandFBGfile.close()
+    def __init__(self):
+        # Open the file we will write to, and keep it in our object's scope.
+        self.csvNFLandFBGfile = open("NFL and FBG.csv", "wb")
+        # Create our list of field names.
+        listFieldNames = NFLPlayer.fields.keys()
+        # Create our DictWriter with the list of field names as the header row.
+        self.writerPlayerAttributeDicts = csv.DictWriter(self.csvNFLandFBGfile, listFieldNames)
+        # Write the header first.
+        self.writerPlayerAttributeDicts.writeheader()
+        # We're done here. The rest of the writing will be done per-item in process_item.
+    
+    def process_item(self, item, spider):
+        # Write this player item as a dict.
+        self.writerPlayerAttributeDicts.writerow(dict(item))
+        # Per the docs here: 
+        #    http://doc.scrapy.org/en/latest/topics/item-pipeline.html#process_item 
+        # we must either return the item or raise a DropItem execption.
+        return item
+    
+    def spider_closed():
+        # Just close the file.
+        self.csvNFLandFBGfile.close()
